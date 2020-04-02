b0e008da75111b09a5dfe39d9cabef2c57578fde
wz <wangzhou8284@outlook.com>
2020-04-02 10:14:02 +0800

    route 4: second route outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,14 @@ import {RouterModule, Routes} from '@angular/router';
 
 /* . . . */
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {ComposeMessageComponent} from './compose-message/compose-message.component';
 
 const appRoutes: Routes = [
+  {
+    path: 'compose',
+    component: ComposeMessageComponent,
+    outlet: 'popup'
+  },
   {path: '', redirectTo: '/heroes', pathMatch: 'full'},
   {path: '**', component: PageNotFoundComponent}
 ];
